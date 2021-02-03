ea484c4702d4e3c57fa08e64f059f9a183f0ee0a
igorek240101 <48420254+igorek240101@users.noreply.github.com>
2021-02-03 23:41:45 +0300

    init

---
+++ C_1_base/C_1_base/YourTaksHere.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_1_base
+{
+    static class YourTaksHere
+    {
+
+
+        #region Задача 1
+        /*
+         * На вход подаются два катета прямоугольного тругольника требуется найти площадь этого тругольника
+        */
+
+        public static double SquerOfTrungle(uint a, uint b)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+
+        #region Задача 2
+        /*
+         * На вход подаются длины трех отрезков, верните true, если они могут образовать тругольник, в обратном случае верните false
+         * 
+         * Условие существования треугольника - a*a меньше или равно b*b + c*c, должно выполняться для любой a 
+        */
+
+        public static bool isTrungle(uint a, uint b, uint c)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+
+        #region Задача 3
+        /*
+         * На вход подается два числа, если числа имеют одинаковые знаки, необходимо вывести сумму, иначе разность, взятую по модулю
+        */
+
+        public static int Arifmetika(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+
+        #region Задача 4
+        /*
+         * На вход подается число, необходимо найти и вернуть факториал этого числа
+        */
+
+        public static int Fuc(int a)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+
+        #region Задача 5
+        /*
+         * На вход подается число n, необходимо создать массив длинной n и проинициализровать его значениями n, n-1 ... 1 (гарантируется, что n >= 1)
+        */
+
+        public static int[] ArrayCreate(int n)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+
+        #region Задача 6
+        /*
+         * На вход подается массив чисел, необходимо вернуть индекс первого вхождения наибольшего элемента массива
+        */
+
+        public static int MaxSearch(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+
+        #region Задача 7
+        /*
+         * На вход подается массив чисел, необходимо вернуть его полную копию
+        */
+
+        public static int[] MaxSearc(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+
+        #region Задача 8
+        /*
+         * Реализуйте рекурсивную функцию нахождения чисел Фибоначи (a(n) = a(n-1) + a(n-2), где a(1) - 1, a(2) - 2)
+        */
+
+        public static int Fibonachi(int n)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+
+        #region Задача 9
+        /*
+         * На вход подается массив, необходимо найти наибольшее значение в нем (рекомендуется использовать функцию из задания 6),
+         * а так же вернуть полную копию входного массива с помощью модификатора out (рекомендуется использовать функцию из задания 7)
+        */
+
+        public static int HardArray(int[] intput, out int[] output)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+
+        #region Задача 10
+        /*
+         * Вы создаете боевую систему для компьютерной игры
+         * Вам необходимо написать метод, который расчитывает результаты боя
+         * На каждом ходу игроки наносят урон противнику равный их силе
+         * Бой заканчивается, когда у одного из игроков (или у обоих) здоровье опуститься до 0 или ниже
+        */
+
+        public static void Fight(ref int health1, int power1, ref int health2, int power2)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
